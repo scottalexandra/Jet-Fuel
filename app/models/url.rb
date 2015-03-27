1a81910706d6f6e40dd4e948af4148faaf89e8db
Alex Robinson <alexandra.scott335@gmail.com>
2015-03-27 14:09:47 -0600

    fixed self in url model

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,6 +1,6 @@
 class Url < ActiveRecord::Base
   def create_short_url
     rand_part = Digest::SHA256.hexdigest(long)[0..3]
-    short = "https://al.x/" + rand_part
+    self.short = "https://al.x/" + rand_part
   end
 end
