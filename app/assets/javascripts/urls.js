190ef0f105862e210290a482b81d044196857fcd
Alex Robinson <alexandra.scott335@gmail.com>
2015-03-29 22:49:35 -0600

    formatted url index page

--- app/assets/javascripts/urls.js
+++ app/assets/javascripts/urls.js
@@ -1,25 +1,23 @@
 $(document).ready(function(){
-
-  toggleCreatedAt("#show-created-at",
+  toggleDiv("#show-created-at",
                   "#created-at",
                   "#popular",
                   "#show-popular");
-  togglePopular("#show-popular",
+  toggleDiv("#show-popular",
                 "#popular",
                 "#created-at",
                 "#show-created-at");
 
+  // ("#search").keyup(function() {
+    // var search = $(this).val();
+    // $("td").show();
+    // if (search) $("td").not(":contains("+ term +")").hide();
+  // });
 
-  function toggleCreatedAt(buttonID, divID, divID2, buttonID2){
-    $(buttonID).on("click", function(){
-      $(divID2).addClass("hidden");
-      $(divID).removeClass("hidden");
-      $(buttonID).addClass("colorize");
-      $(buttonID2).removeClass("colorize");
-    });
-  }
 
-  function togglePopular(buttonID, divID, divID2, buttonID2){
+
+
+  function toggleDiv(buttonID, divID, divID2, buttonID2){
     $(buttonID).on("click", function(){
       $(divID2).addClass("hidden");
       $(divID).removeClass("hidden");
