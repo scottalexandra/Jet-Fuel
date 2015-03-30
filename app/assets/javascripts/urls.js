5f6c5536ca36c1855d4346673a650b9f6f10a863
Alex Robinson <alexandra.scott335@gmail.com>
2015-03-29 19:07:15 -0600

    fixing hound issues

--- app/assets/javascripts/urls.js
+++ app/assets/javascripts/urls.js
@@ -1,22 +1,28 @@
 $(document).ready(function(){
-  toggleCreatedAt("#show-created-at", "#created-at", "#popular", "#show-popular")
-  togglePopular("#show-popular", "#popular", "#created-at", "#show-created-at")
+  toggleCreatedAt("#show-created-at",
+                  "#created-at",
+                  "#popular",
+                  "#show-popular");
+  togglePopular("#show-popular",
+                "#popular",
+                "#created-at",
+                "#show-created-at");
 
   function toggleCreatedAt(buttonID, divID, divID2, buttonID2){
-    $(buttonID).on('click', function(){
-      $(divID2).addClass('hidden');
-      $(divID).removeClass('hidden');
-      $(buttonID).addClass('colorize');
-      $(buttonID2).removeClass('colorize');
+    $(buttonID).on("click", function(){
+      $(divID2).addClass("hidden");
+      $(divID).removeClass("hidden");
+      $(buttonID).addClass("colorize");
+      $(buttonID2).removeClass("colorize");
     });
-  };
+  }
 
   function togglePopular(buttonID, divID, divID2, buttonID2){
-    $(buttonID).on('click', function(){
-      $(divID2).addClass('hidden');
-      $(divID).removeClass('hidden');
-      $(buttonID).addClass('colorize');
-      $(buttonID2).removeClass('colorize');
+    $(buttonID).on("click", function(){
+      $(divID2).addClass("hidden");
+      $(divID).removeClass("hidden");
+      $(buttonID).addClass("colorize");
+      $(buttonID2).removeClass("colorize");
     });
-  };
+  }
 });
